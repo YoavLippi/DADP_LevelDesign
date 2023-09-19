6b7d0dd8838d0d99a860dff3d6a71c3f093c0524
CayCall <131669034+CayCall@users.noreply.github.com>
2023-09-19 13:30:12 +0200

    Implemented a crouch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,23 @@
+using Unity.VisualScripting;
 using UnityEngine;
  
 public class PlayerController : MonoBehaviour
 {
     CharacterController characterController;
-    [SerializeField] private float playerSpeed =1;
+    [SerializeField] private float playerSpeed = 5;
     [SerializeField] private float gravityValue = 9.8f;
     [SerializeField] private float jumpHeight;
     private float velocity = 0;
     private bool groundedPlayer;
     private Vector3 playerVelocity;
+
+    #region crouch
+        private Vector3 crouchScale = new Vector3(1, 0.5f, 1);
+        private Vector3 playerScale = new Vector3(1, 1f, 1);
+
+
+
+        #endregion
  
     private void Start()
     {
@@ -50,10 +59,31 @@ public class PlayerController : MonoBehaviour
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         characterController.Move(playerVelocity * Time.deltaTime);
+        
+        #region Handles Crouch
+        
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                playerSpeed = 2.1f;
+                transform.localScale = crouchScale;
+                transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+                
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                playerSpeed = 5.5f;
+                transform.localScale = playerScale;
+                transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+            }
+    
+
+        #endregion
     }
 
     private Vector3 MultiplyComponents(Vector3 a, Vector3 b)
     {
         return new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
     }
+
 }
