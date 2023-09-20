b3c239af168953018cc46fa84528769e0b66e6b9
CayCall <131669034+CayCall@users.noreply.github.com>
2023-09-21 01:56:54 +0200

    Revert "EMP system touchups"
    This reverts commit 2485c8a6853d2bbb4bd302cc20c7745cd9092937.

--- Assets/Scripts/Items/EMP.cs
+++ Assets/Scripts/Items/EMP.cs
@@ -5,30 +5,5 @@ using UnityEngine;
 
 public class EMP : MonoBehaviour
 {
-    public float radius = 20f;
-    public LayerMask targetLayer; 
-
-    private void Start()
-    {
-        Vector3 startLocation = transform.position; 
-        Collider[] colliders = Physics.OverlapSphere(startLocation, radius, targetLayer);
-
-        foreach (Collider collider in colliders)
-        {
-           
-            GameObject hitObject = collider.gameObject;
-            if (hitObject.CompareTag("Enemy"))
-            {
-                Destroy(hitObject);
-              
-                // hitObject.GetComponent<MyScript>().enabled = false; // use this to disable enemy script - walking and detection
-            }
-
-            if (hitObject.CompareTag("BreakableDoor"))
-            {
-                Destroy(hitObject);    
-            }
-        }
-    }
-}               
-
+               
+}
